40283733cc2793b463100b124450fd624c5f4971
Gaurav Sablok <sablokg@gmail.com>
2024-03-12 11:45:22 +0100

    Update pacbiohifi_up.sh

--- pacbiohifi_up.sh
+++ pacbiohifi_up.sh
@@ -1,19 +1,24 @@
+#!/usr/bin/bash 
 # an entire workflow for the running of the high throughput cluster on the analysis of the genomes
 # coming from the pacbiohifi using the verkko, hifiasm, genomeasm4pg
 # below is a complete slurm configuration for the same
 # Author Gaurav Sablok
 # Universitat Potsdam
 # Date: 2024-3-11
-# please change the variables or else use the version tomorrow which has the complete support for the auxillary analysis also.  
-##########
-read -r -p "please provide the username:" username
+##################
+                     echo "there are two assembly configurations available, either perform the assembly"
+            echo "on the single reads or perform the assembly using the paternal and the maternal sequences"
+            echo "you can easily integrate this into the snakemake patterns"
 read -r -p "please select the option:" option
-read -r -p "please provide the path to the directory on the slurmserver:" directory
-read -r -p "please provide the path to the scratch directory on the slurmserver:" scratch
+read -r -p "please provide the path to the directory on the slurmserver where the fastq files are located:" directory
+read -r -p "please provide the path to the scratch directory on the slurmserver where the fastq files will be analyzed:" scratch
 read -r -p "please provide the file with the links to the external fetch:" fetchfile
-if [[ "${option}" == "all" && \
-		        ${directory} && ${scratch} \
-				              && ${username} ]]; then
+read -r -p "please select the assembly configuration:" configuration
+
+if [[ ${configuration} == "singlereads" && "${option}" == "all" && \
+		        ${directory} && ${scratch} ]]; then
+
+echo "writing the sbatch configuration for the slurmruns"
 echo #!/bin/bash
 echo #SBATCH --partition=all
 echo #SBATCH --nodes=1
@@ -27,7 +32,7 @@ echo #SBATCH --output=slurm-%j.out
 module list 
 module load lang/Anaconda3/2021.05
 ### moving the files for the analysis
-cp -r ${directory}/*.fastqgz ${scratch} 
+cp -r ${directory}/*.fastq.gz ${scratch} 
 cd ${scratch}
 for i in *.gz; do gunzip "${i}"; done
 ### creating and activating the conda environment
@@ -42,7 +47,7 @@ conda clean -t
 echo "the environment for the analysis have been created and modified"
 source activate verkko
 declare -a files=()
-for i in ls -l *.fastq.gz
+for i in $(ls -l *.fastq.gz)
   do 
      files+=("${i}")
   done
@@ -65,9 +70,10 @@ for i in files
  done 
 echo "the hifiassembly using the hifiasm has been completed" 
 echo "thank you for using the hifi cluster computing services"
-elif [[ "${option}" == "all" && \
-		     ${directory} && ${scratch} \
-				              && ${username} && ${fetchfile} ]]; then
+
+elif [[ ${configuration} == "singlereads" && "${option}" == "all" && \
+		        ${directory} && ${scratch} && ${fetchfile} ]]; then
+echo "writing the configuration files for the slurmruns and fetching the files from the fetch link provided"
 echo #!/bin/bash
 echo #SBATCH --partition=all
 echo #SBATCH --nodes=1
@@ -80,9 +86,10 @@ echo #SBATCH --mail-type=ALL
 echo #SBATCH --output=slurm-%j.out
 module list
 module load lang/Anaconda3/2021.05
-
+wget -F "${fetchfile}" -o links.txt
 declare -a fetchfiles=()
-cat ${fetchfiles} | while read line; do 
+cat links.txt | while read line
+   do 
       fectchfiles += (${line})
    done
 for i in fetchfiles
@@ -96,7 +103,6 @@ for i in "${scratch}"/"${i%.**}"/assembly.fasta
    done
    echo "the assembly files for the verkko have been moved and assembled and you can find them in the address"
    echo "${scratch}/fasta_files_assembly"
-
 echo "starting out with the hifiasm assembly"
 for i in files
  do 
@@ -104,4 +110,44 @@ for i in files
  done 
 echo "the hifiassembly using the hifiasm has been completed" 
 echo "thank you for using the hifi cluster computing services"
-fi 
+elif [[ ${configuration} == "parents" && "${option}" == "all" && \
+		        ${directory} && ${scratch} ]]; then
+read -r -p "please provide the path to the maternal sequences:" maternal
+read -r -p "please provide the path to the paternal sequences:" paternal
+read -r -p "please provide the path to the child ones:" child
+read -r -p "please provide the kmer for the estimation of the trio binning method:" selectedkmer
+read -r -p "do you want to evaluate the different kmers for the trio binning method:" different
+if [[ "${selectedkmer}" && "${different}" == "" ]]; then
+mkdir makingkmers
+cd makingkmers
+meryl count compress k=30 threads="${cpus-per-task}" memory="${mem}" "${maternal}"/*.fastq.gz output maternalkmers."${selectedkmer}".meryl
+meryl count compress k=30 threads="${cpus-per-task}" memory="${mem}" "${paternal}"/*.fastq.gz output paternalkmers."${selectedkmer}".meryl
+meryl count compress k=30 threads="${cpus-per-task}" memory="${mem}" "${child}"/*.fastq.gz output paternalkmers."${selectedkmer}".meryl
+cd ..
+echo "cloning merqury for making the hapmap datasets"
+git clone https://github.com/marbl/merqury.git
+sh $(pwd)/merqury/hapmers.sh $(pwd)/makingkmers/maternalkmers."${selectedkmer}".meryl \
+                                     $(pwd)/makingkmers/maternalkmers."${selectedkmer}".meryl \ 
+                                                   $(pwd)/makingkmers/maternalkmers."${selectedkmer}".meryl
+elif [[ "${selectedkmer}" && ${different} == "yes" ]]; then
+read -r -p "how many kmers:" kmers
+declare -a arraystorage=()
+while [[ "${kmers}" -le "${#arraystorage[*]}" ]]; 
+do
+  read -r -p "please enter the kmers:" kmer
+   arraystorage+=("${kmer}")
+  if [ "${#arraystorage[*]}" -eq "${kmers}" ]; then
+   break
+ fi  
+done
+touch storingvariablekmers.sh
+for i in ${#arraystorage[*]}; 
+   do 
+      echo meryl count compress k="${i}" threads="${cpus-per-task}" memory="${mem}" "${maternal}"/*.fastq.gz output maternalkmers."${i}".meryl
+      echo meryl count compress k="{i}" threads="${cpus-per-task}" memory="${mem}" "${paternal}"/*.fastq.gz output paternalkmers."${i}".meryl
+      echo meryl count compress k="{i}" threads="${cpus-per-task}" memory="${mem}" "${child}"/*.fastq.gz output paternalkmers."${i}".meryl
+   done >> sotringvariablekmers.sh 
+
+if [[ -z $(cat storingvariablekmers.sh | wc -l ) ]]; then
+   echo "variables are not properly formatted and stored"
+fi
